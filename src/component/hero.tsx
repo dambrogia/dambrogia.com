6de546949418f35bba4eb3b996bcbd9475ac4f34
Dominic dambrogia <dominic.dambrogia@palmettostatearmory.com>
2023-03-10 15:31:37 -0800

    lint

--- src/component/hero.tsx
+++ src/component/hero.tsx
@@ -1,10 +1,10 @@
 export default function Hero() {
-  return <div className="c-hero relative xl:!bg-center  bg-cover" style={{ backgroundImage: 'url(/home/dominic-dambrogia.jpeg)', filter: 'grayscale(25%)', backgroundPosition: '90% 50%' }}>
-    <div className="w-full h-full" style={{ background: 'rgba(0,0,0,0.4)' }}>
+  return <div className="c-hero relative xl:!bg-center  bg-cover" style={{backgroundImage: 'url(/home/dominic-dambrogia.jpeg)', filter: 'grayscale(25%)', backgroundPosition: '90% 50%'}}>
+    <div className="w-full h-full" style={{background: 'rgba(0,0,0,0.4)'}}>
       <div className="u-container py-72" >
         <div className="u-row">
           <div className="u-col w-full text-center">
-            <h1 className='text-5xl mb-12 font-bold'>Dominic D'Ambrogia</h1>
+            <h1 className='text-5xl mb-12 font-bold'>Dominic D&apos;Ambrogia</h1>
             <h3 className='text-2xl mb-1 font-thin'>Principal Software Architect</h3>
             <h5 className='text-lg font-thin'>&copy; Palmetto State Armory</h5>
           </div>
