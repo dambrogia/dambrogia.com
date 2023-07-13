6b21195c723c51fbc43a78ec4a7782376d409a37
Dominic dambrogia <dominic.dambrogia@palmettostatearmory.com>
2023-07-13 10:02:25 -0700

    mailer wip

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,14 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from 'next'
+import mailer from '@sendgrid/mail'
 
 type Data = {
-  data: any
+  data: {
+    status: 'failure' | 'success';
+    message: string;
+  }
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log(req.body)
+  console.log(req)
   res.status(200).json({data: req.body})
+
+  const config = {
+    apiKey: process.env.SENDGRID_API_KEY as string,
+    mailTo: process.env.MAIL_TO as string,
+    mailSubject: process.env.MAIL_SUBJECT as string,
+  }
+
+  const msg = {
+    to: config.mailTo,
+    from: req.body.email,
+    subject: config.mailSubject,
+    text: req.body.content,
+  }
+
+  const makeData = (s: 'success' | 'failure', m: string): Data => ({data: {status: s, message: m}})
+
+  try {
+    const mailerResp = await mailer.send(msg)
+    res.status(201).json(makeData('success', 'Contact submission was successful.'))
+  } catch (e) {
+    res.status(201).json(makeData('failure', 'Contact submission failed.'))
+  }
 }
+
+
